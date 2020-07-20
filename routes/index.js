ae4ef648a22504bad6b409f90fe1383f964bf9a3
Kevin Lee <kevinlee3627@gmail.com>
2020-07-19 21:32:20 -0500

    Changed res.render to res.sendfile of build

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ const path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // console.log(__dirname);
-  // res.sendFile(path.join(__dirname, '/../build', 'index.html'));
-  res.render('index', { title: 'Express' });
+  res.sendFile(path.join(__dirname, '/../build', 'index.html'));
+  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
