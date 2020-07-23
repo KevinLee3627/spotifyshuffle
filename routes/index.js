24924dae315f8e000a94c8d805f4759436698bbd
Kevin Lee <kevinlee3627@gmail.com>
2020-07-23 17:10:59 -0500

    Changed index.js route in Express to hopefully get react routing working, changed api.js to accept /api instead of /*

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('*', function(req, res, next) {
+  console.log('GETTING INDEX!');
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 });
 
