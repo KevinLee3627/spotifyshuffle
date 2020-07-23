52f71dc72edc0dce06a860a4d53f16bef82806a3
Kevin Lee <kevinlee3627@gmail.com>
2020-07-23 17:02:06 -0500

    Added static.json to react home directory in attempt to get react routing working in Heroku

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,7 @@ const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  // console.log(__dirname);
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  // res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
